b1b6d51ee3916fe90bc0e1f826d8db31a027f9a9
jxt5860269 <2510793495@qq.com>
2019-06-17 18:49:14 +0800

    creat project

---
+++ git/src/com/woniu/jt/pojo/UserPO.java
@@ -0,0 +1,35 @@
+package com.woniu.jt.pojo;
+
+import java.io.Serializable;
+
+public class UserPO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String account;
+	private String password;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "UserPO [id=" + id + ", account=" + account + ", password=" + password + "]";
+	}
+}
